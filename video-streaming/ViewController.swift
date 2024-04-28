8b063487932cdbb6502c5f12a272a8ae3e3ea394
영현 <yh97yhyh@naver.com>
2024-04-29 02:27:12 +0900

    first commit

---
+++ video-streaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video-streaming
+//
+//  Created by 영현 on 4/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
